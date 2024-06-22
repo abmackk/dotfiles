7a68f689e8530f542123df29211475a1c53c6d0b
abmackk <abhi.mack3@gmail.com>
2024-06-22 14:24:41 +0530

    ayu theme for nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
+-- return {
+--   "craftzdog/solarized-osaka.nvim",
+--   lazy = true,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent = true,
+--     }
+--   end,
+-- }
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+  "shatur/neovim-ayu",
 }
